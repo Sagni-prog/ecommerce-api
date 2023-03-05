365d8bb7446b46ee48fdbbb72c629c2bd2df83ee
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-05 15:30:11 +0300

    api route created

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,51 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/products', function () {
+
+    $products = App\Models\Product::with('photos','catagory')->get();
+
+
+        foreach($products as $product){
+            foreach($product->photos as $photo){
+                // return $photo;
+            }
+        }
+
+        return $products;
+
+
+});
+
+
+Route::get('/catagories', function () {
+
+
+   $catagory = \App\Models\Catagory::with('products')->whereHas('products',function($q){
+        $q->with('photos');
+   })->get();
+
+  
+  return $catagory;
+});
+
+Route::get('/order',function(){
+//    $order = App\Models\orderProduct::with('order')->get();
+// $order = App\Models\Product::with('orderProduct')->get();
+
+
+    //  return $order->order;
+
+    $o = App\Models\orderProduct::with('products','order')->find(1);
+      foreach($o->products as $product){
+        echo $product->price;
+        echo "<br/>";
+      }
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -17,50 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-
-    $products = App\Models\Product::with('photos')->get();
-
-
-        foreach($products as $product){
-            foreach($product->photos as $photo){
-                echo $photo->photo_name;
-                echo "<br/>";
-
-            }
-        }
-});
-
-
-Route::get('/catagories', function () {
-
-    $catagory = App\Models\Catagory::find(3)->products()->get();
-
- foreach ($catagory as $product) {
-    
- }
-   $catagory = \App\Models\Catagory::all();
-//    return $catagory;
-
-  $p = App\Models\Product::with('catagory')->get();
-  
-  return $p;
-//    return $catagory->products;
-});
-
-Route::get('/order',function(){
-//    $order = App\Models\orderProduct::with('order')->get();
-// $order = App\Models\Product::with('orderProduct')->get();
-
-
-    //  return $order->order;
-
-    $o = App\Models\orderProduct::with('products','order')->find(1);
-      foreach($o->products as $product){
-        echo $product->price;
-        echo "<br/>";
-      }
-});
 
 
 // Auth::routes();
