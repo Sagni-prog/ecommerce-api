a739e34f7d531a38d085237545145a6e46ee4c26
Natnael Getachew <natnaeln4d@gmail.com>
2023-03-07 08:34:47 +0300

    checking

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,8 @@ use App\Http\Controllers\Auth\AuthController;
 
 
 Route::post('login',[AuthController::class,'login']);
-<<<<<<< HEAD
 Route::post('/register',[\App\Http\Controllers\Auth\AuthController::class,'register']);
 
-=======
->>>>>>> 22436fe491606d585c2386f3fab1969cd7a1cfd2
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
