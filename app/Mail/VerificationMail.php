b9f1a7029f1b0e5d79b1b7a525e2e4805eb386fd
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-07 11:11:20 +0300

    verification email starter kit

--- app/Mail/VerificationMail.php
+++ app/Mail/VerificationMail.php
@@ -8,44 +8,53 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Mail\Mailables\Address;
 
-class ResetPassword extends Mailable
+class VerificationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-   
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
 
-    public $data;
-
+    protected $data;
 
     public function __construct($data)
     {
         $this->data = $data;
     }
 
-  
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
     public function envelope()
     {
         return new Envelope(
-            from: new Address('sagnialemayehu69@gmail.com','Sagni'),
-            subject: 'Reset Password',
+            subject: 'Verification Mail',
         );
     }
 
-    
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
     public function content()
     {
         return new Content(
-            view: 'emails.reset_password',
-            with: [
-                'data' => $this->data
-            ]
-           
+            view: 'view.name',
         );
     }
 
-   
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
     public function attachments()
     {
         return [];
--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -1,6 +1,6 @@
 <p>
-    Hi, <br>
-    You have new conatact email from your website.<br><br>
+    Hi There! <br>
+    hello this the email from unknown source, dont be afraid .<br><br>
    
     <strong>Pin :</strong>{{$data['pin']}}<br>
      
